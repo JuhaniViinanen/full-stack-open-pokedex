bf1c76803ef4ac2043f244b630a16691e5822668
Juhani Viinanen <juhani.ilmari.viinanen@gmail.com>
2024-08-16 18:10:54 +0300

    simulated broken deployment

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (_req, res) => {
-  res.send('1')
+  res.send('broken')
 })
 
 app.get('/health', (_req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('healthcheck error')
   res.send('ok')
 })
 
