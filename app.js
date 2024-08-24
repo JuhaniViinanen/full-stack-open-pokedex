07d3bea9f0a2044b4e8d6d18f744f6c82d7ab0ce
Juhani Viinanen <juhani.ilmari.viinanen@gmail.com>
2024-08-24 21:00:42 +0300

    pushing a broken branch

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (_req, res) => {
-  res.send('11.16 #skip')
+  res.send('11.17 testing')
 })
 
 app.get('/health', (_req, res) => {
@@ -15,6 +15,8 @@ app.get('/health', (_req, res) => {
 })
 
 app.listen(PORT, () => {
+  throw new Error("this will fail");
+  
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
