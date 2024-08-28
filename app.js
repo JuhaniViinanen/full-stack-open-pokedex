7ab802f98b71fbf627b372f86e2762a6334efa2b
Juhani Viinanen <juhani.ilmari.viinanen@gmail.com>
2024-08-28 19:52:18 +0300

    testing pushing a broken build

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.get('/health', (_req, res) => {
 })
 
 app.listen(PORT, () => {
+  throw new Error("broken on purpose")
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
